bd1f84e631939418f01b467d706015037cb61bd8
Rafaell Lycan <sonny.webdsg@gmail.com>
2017-09-01 15:43:23 +0200

    Add dimension validation on Matrix

--- src/Abaco/Matrix.js
+++ src/Abaco/Matrix.js
@@ -15,16 +15,28 @@ export class Matrix {
                 throw new Error('Matrix line must be an array');
             }
 
-            this.validateLineElement(line);
+            this.validateLineColumn(line);
         });
+
+        this.validateDimension(lines);
     }
 
-    validateLineElement(line) {
+    validateLineColumn(line) {
         line.forEach( el => {
             if(!Number.isInteger(el)) {
                 throw new Error('Matrix only supports integer elements');
             }
         });
         
+    }
+
+    validateDimension(lines) {
+        lines.reduce((dimension, column) => {
+            if(dimension && column.length !== dimension) {
+                throw new Error(`Dimension mismatch (${dimension} != ${column.length})`);
+            }
+            dimension = column.length;
+            return dimension;
+        }, 0);
     }
 }
--- test/Abaco/Matrix.spec.js
+++ test/Abaco/Matrix.spec.js
@@ -13,6 +13,10 @@ describe('Matrix', () => {
     it('should throw an Error when line is not an array', () => {
         assert.throws(() => new Matrix([1,2,3]), /Matrix line must be an array/);
     });
+
+    it('should throw an Error when dimensions does not match', () => {
+        assert.throws(() => new Matrix([[1,2,3], [1,2]]), /Dimension mismatch \(3 != 2\)/);
+    });
 });
 
 function invalidLineElementData() {
